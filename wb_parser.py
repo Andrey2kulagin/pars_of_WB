ec87a88144cf11c46d1bd4ab055f38637d578aa4
andrey2kulagin <andrey2kulagin@yandex.ru>
2022-05-10 16:59:18 +0300

    Переписал парсер в файле wb_parser. Всё работает, далее буду использовать сначала ооп, потом сделаю многопоточность, у меня очень слабый ноутбук, поэтому перевожу парсер в фоновый режим, в фоновом режиме не работают расширения и всё, что касается выручки вместе с ним. Сейчас парсер будет на более высоком уровне с урезанным функционалом

--- wb_parser.py
+++ wb_parser.py
@@ -42,6 +42,61 @@ def search(driver, input):  # 5
     inputLine.send_keys(Keys.ENTER)
     print("нажат энтер")
 
+#нахождение кол-ва карточек
+def getCount(driver_):  # 6
+    print("6")
+    print("ждём загрузки кол-ва товаров")
+    WebDriverWait(driver_, 50).until(EC.presence_of_element_located((By.XPATH, "//*[@class='goods-count']/span[1]")))
+    print("кол-во товаров загружено")
+    while driver_.find_element(By.XPATH, "//*[@class='goods-count']/span[1]").text=="":
+        continue
+    cards_count_element = driver_.find_element(By.XPATH, "//*[@class='goods-count']/span[1]")
+    print("кол-во товаров получено")
+    cards_count = cards_count_element.text.replace(" ","")
+    return int(cards_count)
+
+# получение интового массива с ценами
+def getPrices(driver):  # 10
+    print("#10")
+    print("ждём, пока цены загрузятся")
+    WebDriverWait(driver, 50).until(
+        EC.presence_of_element_located((By.XPATH, "//*[@class='lower-price']")))
+    priceslist = driver.find_elements(By.XPATH, "//*[@class='lower-price']")
+    print("нашли цены")
+    prices = []
+    print("#11")
+    print("начинается обработка массива цен")
+    for price in priceslist:  # 11
+        prices.append(int(price.text.replace(" ", "").replace("₽", "")))
+    return prices
+
+# получение интового массива с выручкой
+def getEarnings(driver):  # 12
+    print("#12")
+    print("ждём закгрузки блока маяка")
+    time.sleep(6)
+    element = driver.find_element((By.TAG_NAME,"body"))
+    element.screenshot("screenshot_full.png")
+    WebDriverWait(driver, 50).until(
+        EC.presence_of_element_located((By.XPATH, "//*[@class='mayak-details mayak-details--in-card']/div[1]")))
+    print("ждём загрузки значений в маяк")
+    while driver.find_elements(By.XPATH, "//*[@class='mayak-details mayak-details--in-card']/div[1]")[0]=="":
+        continue
+    print("находим эти цены")
+    earnings = driver.find_elements(By.XPATH, "//*[@class='mayak-details mayak-details--in-card']/div[1]")
+    earnings_arr = []
+    for earning in earnings:  # 13
+        print(earning.text)
+        print("#13")
+        # if earning.text.replace("\u202f", "").split(" ")[1] == "-" or earning.text.replace("\u202f", "").split(" ")[1] == "0" :  # 7
+        #     print("#7")
+        #     earnings_arr.append(0)
+        # else:
+        #     earnings_arr.append(int(earning.text.replace("\u202f", "").split(" ")[1]))
+        earn = earning.text.replace(" ","").replace("р.","")
+        print(earn.find(" "))
+        earnings_arr.append(earn)
+    return earnings_arr
 
 #Главная часть!!
 # настройки опшион
@@ -53,14 +108,17 @@ chrome_options.add_experimental_option("useAutomationExtension", False)
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument("--window-size=1920,1080")
-#chrome_options.headless = True # фоновый режим браузера
-# установка расширения
 extension_path = "C:\my programs\e5.0.4_0.crx"  # поменять расположение расширения
 chrome_options.add_extension(extension_path)
+chrome_options.headless = True # фоновый режим браузера
+# установка расширения
+
 # создание браузера
 driver = webdriver.Chrome(options=chrome_options)
 #запуск парсера
 login(driver)#здесь всё работает отлично
-#теперь надо научиться вводить запросы в поисковую строку
 input = "фонарик"
-search(driver,input)#всё хорошо
+search(driver,input)#поиск нужного товара
+cards_count = getCount(driver)#nахождение кол-ва карточек
+prices_arr = getPrices(driver)#нахождение массива цен
+#print(getEarnings(driver))
